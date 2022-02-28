84feec6d6c5f6af29c732326b360af4eaca246f3
Nick Fleming <buckonick@gmail.com>
2022-03-01 10:55:37 +1100

    Floating-Point Expressions

---
+++ testing zylabs/part 18/floating-point expression/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int x;
+   double y;
+
+   cin >> x;
+
+   y = (1.0 / 3) * x + (x / 4.0) + 2 * x;
+   
+   cout << y << endl;
+
+   return 0;
+}
