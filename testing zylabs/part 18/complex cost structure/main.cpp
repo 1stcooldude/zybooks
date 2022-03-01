6a81651508e09e4bfe5d5c79501f0ea1bdcc7a16
Nick Fleming <buckonick@gmail.com>
2022-03-01 11:11:51 +1100

    Complex Cost Structure

---
+++ testing zylabs/part 18/complex cost structure/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int passengerAge;
+   int carryOns;
+   int checkedBags; 
+   int airFare = 0; 
+   
+   cin >> passengerAge;
+   cin >> carryOns;
+   cin >> checkedBags; 
+
+   if (passengerAge >= 60){
+      airFare += 290;
+   } else if (passengerAge > 2){
+      airFare += 300;
+   }
+   
+   if (carryOns == 1){
+      airFare += 10;
+   }
+
+   if (checkedBags == 2){
+      airFare += 25;
+   } else if (checkedBags >= 3){
+      airFare += 50 * (checkedBags - 2) + 25;
+   }
+   
+   cout << airFare << endl;
+
+   return 0;
+}
