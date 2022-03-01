1a1f5d07b9eb172124a3a0003e077f4ff0150b7a
Nick Fleming <buckonick@gmail.com>
2022-03-01 13:04:48 +1100

    Output Rocket Height

---
+++ testing zylabs/part 18/Output rocket height/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int rocketHeight = 0; 
+   int initialVelocity = 0;
+   int timeSinceLaunch = 0; 
+
+   cin >> initialVelocity;
+
+   while (rocketHeight >= 0){
+      cout << timeSinceLaunch << " " << rocketHeight << endl;
+      timeSinceLaunch++;
+      rocketHeight = initialVelocity*timeSinceLaunch - 5 * timeSinceLaunch * timeSinceLaunch;
+   }
+
+   return 0;
+}
