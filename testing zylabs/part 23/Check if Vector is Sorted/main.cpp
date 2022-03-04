a4def2ecaad6cf948d07bfb2b41a985307e086aa
Nick Fleming <buckonick@gmail.com>
2022-03-04 16:03:59 +1100

    Check if Vector is Sorted

---
+++ testing zylabs/part 23/Check if Vector is Sorted/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+bool InOrder(vector<int> nums) {
+	int temp = nums.front();
+   for (int item : nums){
+      if (temp > item){
+         return false;
+      }
+      temp = item;
+   }
+   return true;
+}
+
+int main() {
+
+	vector<int> nums1(5);
+	nums1.at(0) = 5;
+	nums1.at(1) = 6;
+	nums1.at(2) = 7;
+	nums1.at(3) = 8;
+	nums1.at(4) = 3;
+
+
+	if (InOrder(nums1)) {
+		cout << "In order" << endl;
+	}
+	else {
+		cout << "Not in order" << endl;
+	}
+
+	vector<int> nums2(5);
+	nums2.at(0) = 5;
+	nums2.at(1) = 6;
+	nums2.at(2) = 7;
+	nums2.at(3) = 8;
+	nums2.at(4) = 10;
+
+	if (InOrder(nums2)) {
+		cout << "In order" << endl;
+	}
+	else {
+		cout << "Not in order" << endl;
+	}
+
+	return 0;
+}
