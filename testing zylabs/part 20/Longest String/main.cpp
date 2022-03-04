1f206dd9d3f413df7bc719815157dc02c85e94fd
Nick Fleming <buckonick@gmail.com>
2022-03-04 15:27:04 +1100

    Longest String

---
+++ testing zylabs/part 20/Longest String/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str1;
+	string str2;
+	string longest;
+
+   cin >> str1 >> str2;
+
+   if (str1 > str2){
+      longest = str1;
+   } else {
+      longest = str2;
+   }
+
+   cout << longest << endl;
+
+   return 0;
+}
