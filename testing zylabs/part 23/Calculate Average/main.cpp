98eb51e32a663e825d1d915747b68b3453fdc6af
Nick Fleming <buckonick@gmail.com>
2022-03-04 15:57:26 +1100

    Calculate Average

---
+++ testing zylabs/part 23/Calculate Average/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// CalcAverage() takes in an int vector and returns the average value of elements in the vector as a double.
+double CalcAverage(vector<int> nums) {
+   double total;
+   for (int i = 0; i < nums.size(); i++){
+      total += nums.at(i);
+   }
+   return total/nums.size();
+   
+}
+
+int main() {
+   vector<int> nums = {1, 2, 3, 4, 5};
+   cout << CalcAverage(nums) << endl; // CalcAverage() should return 3.0.
+   return 0;
+}
