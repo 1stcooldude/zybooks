a757dd639a4e1d0d92697eb2016fab8325ebdb99
Nick Fleming <buckonick@gmail.com>
2022-03-04 15:23:01 +1100

    Max of 2

---
+++ testing zylabs/part 20/Max of 2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int val1;
+    int val2;
+    int max;
+    
+    cin >> val1 >> val2;
+
+    if (val1 > val2){
+       cout << val1 << endl;
+    } else {
+       cout << val2 << endl;
+    }
+    
+    return 0;
+}
